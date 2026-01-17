16f03f795550e154b4c943c92c287efe565755c5
gurumoorthi-tech <gurumoorthi.tech@gmail.com>
2026-01-17 13:04:51 +0530

    fix : remove section wrapper in contact

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -4,7 +4,6 @@ import { EarthCanvas } from "../canvas";
 import { config } from "../../constants/config";
 import { Header } from "../atoms/Header";
 import { styles } from "../../constants/styles";
-import { SectionWrapper } from "../../hoc";
 // import { Link } from "react-router-dom";
 
 // const INITIAL_STATE = Object.fromEntries(
